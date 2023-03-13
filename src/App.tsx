a327a837561536d6ca929d5bdd1e94d6edf06c2d
Oliver Svetlík <xsvetli1@fi.muni.cz>
2023-03-13 14:32:01 +0100

    Sample app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BugReportingTool, Platform } from "@xsvetli1/bug-reporting-tool";
+import background from "./demo-guess.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BugReportingTool
+      platform={Platform.Github}
+      platformProps={{
+        owner: "xsvetli1",
+        repository: "bug-reporting-tool",
+        authToken: "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",
+      }}
+    >
+      <img src={background} style={{ width: "100%", height: "100vh" }}></img>
+    </BugReportingTool>
+    // <BugReportingTool
+    //   platform={Platform.Gitlab}
+    //   props={{
+    //     server: new URL("https://gitlab.fi.muni.cz/"),
+    //     projectId: 27325,
+    //     authToken: '_X97nsW6RxeEV7EkRHmw'
+    //   }}
+    // >
+    //   <h2>Sample application</h2>
+    //   <p>Lorem ipsum</p>
+    // </BugReportingTool>
   );
 }
 
