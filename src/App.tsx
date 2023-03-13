98bd102686fd7298143575ed207171534d70d5f7
Oliver Svetlík <xsvetli1@fi.muni.cz>
2023-03-13 14:35:54 +0100

    Update auth token

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,28 @@ import background from "./demo-guess.png";
 
 function App() {
   return (
-    <BugReportingTool
-      platform={Platform.Github}
-      platformProps={{
-        owner: "xsvetli1",
-        repository: "bug-reporting-tool",
-        authToken: "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",
-      }}
-    >
-      <img src={background} style={{ width: "100%", height: "100vh" }}></img>
-    </BugReportingTool>
     // <BugReportingTool
-    //   platform={Platform.Gitlab}
-    //   props={{
-    //     server: new URL("https://gitlab.fi.muni.cz/"),
-    //     projectId: 27325,
-    //     authToken: '_X97nsW6RxeEV7EkRHmw'
+    //   platform={Platform.Github}
+    //   platformProps={{
+    //     owner: "xsvetli1",
+    //     repository: "bug-reporting-tool",
+    //     authToken: "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",
     //   }}
     // >
-    //   <h2>Sample application</h2>
-    //   <p>Lorem ipsum</p>
+    //   <img src={background} style={{ width: "100%", height: "100vh" }}></img>
     // </BugReportingTool>
+    <BugReportingTool
+      platform={Platform.Gitlab}
+      platformProps={{
+        server: new URL("https://gitlab.fi.muni.cz/"),
+        projectId: 27325,
+        authToken: "zsRR4zgTxKUv3GzJ1hyJ",
+        // authToken: '_X97nsW6RxeEV7EkRHmw'
+      }}
+    >
+      <h2>Sample application</h2>
+      <p>Lorem ipsum</p>
+    </BugReportingTool>
   );
 }
 
